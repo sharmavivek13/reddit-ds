439aac600a2260415330e565860976672a01b116
Vivek Sharma <sharma_vivek@icloud.com>
2016-11-16 20:39:42 +0530

    updated search.py

--- search.py
+++ search.py
@@ -1,11 +1,10 @@
 '''------------------------------------------------------------------------------
-Basic Reddit Score Predictor by: Vivek Sharma, (sharma_vivek@icloud.com)
+Reddit Post Search by: Vivek Sharma, (sharma_vivek@icloud.com)
 
 DataSet Used : Reddit Top 2.5 Million(https://github.com/umbrae/reddit-top-2.5-million) 
-(Reference from Kaggle Bag of Words)
-Two Models are used for prediction: Xgboost and Random Forest. By default random 
-forest is selected.
-To use xgboost uncomment lines no.76,77,78,92 and comment lines no. 81,82,91
+Program takes a word from user to be searched in a particular subreddit and returns 
+all the posts in that subreddit containing the entered word.
+Currently the results are ranked as per their score.
 For More Info Refer to Readme.md
 ------------------------------------------------------------------------------'''
 
@@ -13,6 +12,7 @@ For More Info Refer to Readme.md
 
 import io
 import re
+import itertools
 import requests
 import pandas as pd
 from flask import Flask
@@ -29,10 +29,10 @@ def my_form():
 @app.route('/', methods=['POST']) 
 def my_form_post():
 
-
 	word=request.form['word']
 	subreddit=request.form['subreddit']
 	main=[]
+	score=[]
 	name=subreddit+".csv"
 	url="https://raw.githubusercontent.com/umbrae/reddit-top-2.5-million/master/data/"+name
 	s=requests.get(url).content
@@ -41,10 +41,14 @@ def my_form_post():
 	for j in range(0,num):
 		if word in train["title"][j]:
 			main.append(train["title"][j])
-	output='\n'.join(map(str, main))
-	return output
-
-###############################################################################
+			score.append(train["score"][j])
+	list_enumerate = itertools.count()
+	main.sort(reverse=True, key=lambda k: score[next(list_enumerate)])
+	if not main:
+		output="No Match Found"
+		return output
+	else:
+		return render_template("temp.html",entries=main)
 
 if __name__ == '__main__':
     app.run(debug=True)
